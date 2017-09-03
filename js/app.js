3580e16c319470725903f4b75503ee2413762866
Travis Henry <travis.s.henry@gmail.com>
2017-09-03 15:42:50 -0700

    Added init function to create initial deck

--- js/app.js
+++ js/app.js
@@ -89,3 +89,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+//initializes the app and creates the initial deck
+function init() {
+    let deck = shuffle(cardDoubler(cardList));
+    drawDeck(deck);
+}
+
+init();
